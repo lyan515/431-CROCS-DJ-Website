382fcb352c4e581aba90a0082e1a226bfbf33559
Ryan Branson <lyan515@tamu.edu>
2018-04-23 03:02:51 +0000

    deleted migrations

--- db/migrate/20180324194018_add_client_approval_to_gig_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddClientApprovalToGigRequests < ActiveRecord::Migration
-  def change
-    add_column :gig_requests, :client_approval, :boolean
-  end
-end
--- db/migrate/20180408214046_add_price_to_gig_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToGigRequests < ActiveRecord::Migration
-  def change
-    add_column :gig_requests, :price, :float
-  end
-end
